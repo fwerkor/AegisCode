1064b0b6ddc75e2f1348a9037bd5279cd0b6335d
Yuhang Cao <caoyuhang@fwerkor.com>
2026-07-07 12:08:11 +0000

    chore: update metadata

--- packaging/homebrew/aegiscode.rb
+++ packaging/homebrew/aegiscode.rb
@@ -1,8 +1,8 @@
 class Aegiscode < Formula
   desc "Opencode-style coding-agent CLI with subagents and governed tools"
   homepage "https://github.com/fwerkor/AegisCode"
-  url "https://github.com/fwerkor/AegisCode/releases/download/v0.1.0/aegiscode-macos-x86_64"
-  version "0.1.0"
+  url "https://github.com/fwerkor/AegisCode/releases/download/v0.2.0/aegiscode-macos-x86_64"
+  version "0.2.0"
   sha256 "PLACEHOLDER"
 
   def install
